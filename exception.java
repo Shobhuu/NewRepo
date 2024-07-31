4a5ab766b2c734758aac01575e7fd2a6a0e8b78a
Shobhuu <saubhagya.agg@gmail.com>
2024-07-31 18:19:43 +0530

    java exceptions

---
+++ exception.java
@@ -0,0 +1,10 @@
+package com.example.demo.work;
+
+//(IOException, RuntimeException)---extends--> Exception class --extends--> Throwable class --extends--> Object class
+//Two types of exception -> 1.Checked Exception(checked at compile time and hence must be handled) --> IOException(need to be mandatory handled in try catch block)
+//e.g. of 1 - IOException, FileNotFoundException(extends IOException)
+//2. Unchecked -->  RuntimeException -- need not be handled in try catch
+//e.g. of 2 - ArithmeticEx, ArrayIndexOutOfBoundsEx, ClassCastEx, IllegalArgumentEx, IllegalStateEx, NullPointerEx, NumberFormatEx
+
+public class exception {
+}
